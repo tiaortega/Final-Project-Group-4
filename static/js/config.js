ef3c6aef88fed5290c98f5905a7325ee0207899e
gelopez21 <gelopez1984@gmail.com>
2021-01-24 22:30:36 -0600

    cleaning

--- static/js/config.js
+++
@@ -1,7 +0,0 @@
-// // 1a693fdf36bd4ee0ecd73ded092a4c8e
-
-// eeGeo_api_key = "0903822487659f81a0471ca69b5eed36"
-
-
-// // pk.eyJ1IjoiamVyZW15bW9za293aXR6IiwiYSI6ImNrM2JveTF2eTBubGYzaXFsNmU0dmtybHoifQ.ijOeYOD-FrnF78IYvD369w
-// mapBox_api_key = "pk.eyJ1IjoiZXJlc2RpdmluYSIsImEiOiJja2hwa2w4OWMwMHVsMnJvOGE3ZDIxdHlwIn0.PfbFm_UP5Rw2C66p0x9Eng"
